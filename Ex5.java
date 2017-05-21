ff2ae0f591d7998a900009c5414298017d5011ad
agagrzyb <agigrzyb@gmail.com>
2017-05-21 22:05:26 +0100

    classes exer

---
+++ Ex5.java
@@ -0,0 +1,162 @@
+package classes;
+import java.io.ObjectInputStream.GetField;
+import javax.sql.rowset.CachedRowSet;
+
+public class Ex5 {
+	// member variables section
+	private int id;						// user id
+	private int ram;					// RAM
+	private int hd;						// Hard Drive
+	private String operatingSystem;		// Operating System
+	private int monitorSize;			// Monitor Size
+	private int cost;					// Cost
+	static int idCounter;				// user idCounter
+
+	// static class variables
+	// Constants for RAM
+	public static final int RAM_4_GB = 4;
+	public static final int RAM_8_GB = 8;
+	public static final int RAM_16_GB =16;
+	//Constants for HARDDRIVE
+	public static final int IDE = 20;
+	public static final int SATA = 21;
+	public static final int SCSI = 22;
+	public static final int SAS = 23;
+	//Constants for MONITORSIZE
+	public static final int SIZE_14 = 14;
+	public static final int SIZE_15 = 15;
+	public static final int SIZE_17 = 17;
+
+	// constructors
+	public Ex5(){
+		id=idCounter;
+		idCounter++;
+	}
+	public Ex5(int ram, int hd, String operatingSystem,
+			int monitorSize){
+		this();
+		setRam(ram);
+		setHd(hd);
+		this.operatingSystem = operatingSystem;
+		setMonitorSize(monitorSize);
+	}
+
+	// methods, setters&getters
+	//RAM
+	public void setRam(int ram){
+		switch(ram){
+		case RAM_4_GB:
+		case RAM_8_GB:
+		case RAM_16_GB:
+			this.ram = ram;
+			break;
+		default:
+			System.out.println("Invalid RAM, setting to RAM 4 GB");
+			this.ram = RAM_4_GB;
+			break;
+		}
+	}
+	public int getRam(){
+		return ram;
+	}
+	//Hard drive
+	public void setHd(int hd){
+		switch(hd){
+		case IDE:
+		case SAS:
+		case SATA:
+		case SCSI:
+			this.hd = hd;
+			break;
+		default:
+			System.out.println("Invalid Hard drive, setting to IDE");
+			this.hd = IDE;
+			break;
+		}
+	}
+	public int getHd(){
+		return hd;
+	}
+	//Operating System
+	public void setOperatingSystem(String operatingSystem){
+		this.operatingSystem = operatingSystem;
+	}
+	public String getOperatingSystem(){
+		return operatingSystem;
+	}
+	//Monitor Size
+	public void setMonitorSize(int monitorSize){
+		switch(monitorSize) {
+		case SIZE_14:
+		case SIZE_15:
+		case SIZE_17:
+			this.monitorSize = monitorSize;
+			break;
+		default:
+			System.out.println("Invalid monitor size, setting to Size 14'");
+			this.monitorSize = SIZE_14;
+			break;
+		}
+	}
+	public int getMonitorSize() {
+		return monitorSize;
+	}
+	//Cost
+	public void setCost(int cost){
+		this.cost = cost;
+	}
+	public int getCost(){
+		return cost;
+	}
+	// get ONLY for user id
+	int getId(){
+		return id;
+	} 
+	//get AsString
+	private String getRamAsString(){
+		switch(ram){
+		case RAM_8_GB:
+			return "RAM 8 GB";
+		case RAM_16_GB:
+			return "RAM 16 GB";
+		default:
+			return "RAM 4 GB";
+		}
+	}
+	private String getHdAsString(){
+		switch (hd) {
+		case SAS:
+			return "SAS";
+		case SATA:
+			return "SATA";
+		case SCSI:
+			return "SCSI";
+		default:
+			return "IDE";
+		}
+	}
+	private String getMonitorSizeAsString(){
+		switch (monitorSize) {
+		case SIZE_15:
+			return "15";
+		case SIZE_17:
+			return "17";
+		default:
+			return "14";
+		}
+	}
+
+
+	// toString
+	public String toString(){
+		return "\nUser number : " + Ex5.idCounter +
+				"\nRAM : " + getRamAsString() +
+				"\nHard drive : " + getHdAsString() +
+				"\nOperating System : " + operatingSystem +
+				"\nMonitor size : " + getMonitorSize() +
+				"\nCost : ";
+
+
+
+	}
+}
