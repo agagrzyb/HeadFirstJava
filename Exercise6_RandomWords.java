02c6defebfc6c9cc660da19e3cae224d8f822c7c
aga <agigrzyb@gmail.com>
2017-06-14 23:07:09 +0100

    Add files via upload

---
+++ Exercise6_RandomWords.java
@@ -0,0 +1,35 @@
+package testing;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise6_RandomWords {
+
+	public static void main(String[] args) {
+		//create my String array
+		String [] myArray = {"Monday", "Tuesday", "Wednesday","Thursday","Friday", "Saturday", "Sunday"};
+		//pick random word from myArray
+		String word = (myArray[new Random().nextInt(myArray.length)]);
+		Scanner scanner =  new Scanner(System.in);
+		//prompt the user...
+		System.out.println("There are " + word.length() + " letters in this randomly selected word. Please guess a letter : ");
+		//set counter to 0
+		int counter = 0;
+		//ask user to try to guess the letter 8 times
+		for(int runEightTimesOnly = 0 ;runEightTimesOnly < 8; runEightTimesOnly++){
+			String input = scanner.nextLine();
+			System.out.println("Letter entered : " + input);
+			//if the match is found...
+			if(word.indexOf(input)!=-1){
+				System.out.println("Letter " + input + " was found at position : " + (word.indexOf(input)+1));	
+			}else{
+				System.out.println(input  + " was not found");	
+				counter++;// increment counter
+			}
+			System.out.println("Your next guess : ");
+			//counter result
+		}System.out.println("Counter of incorect guesses : " + counter);
+		scanner.close();
+		System.out.println("End of work...");
+	}
+}
